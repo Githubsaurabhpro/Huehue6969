3cf60cc0bdb08d85262e6ef74b92d10a42b811e0
Githubsaurabhpro <99381076+Githubsaurabhpro@users.noreply.github.com>
2022-12-23 09:52:17 +0530

    Update inline.py

--- LUCIFER/inline.py
+++ LUCIFER/inline.py
@@ -103,7 +103,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('🌟 Bot Updates 🌟', url="https://t.me/Heaven_Updates")
         ]
         ]
     return InlineKeyboardMarkup(buttons)
